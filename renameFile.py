6c1ad09f97ae4538cb15c9066f78e4bcd9aa27b8
shawnplusplus <46225246+shawnplusplus@users.noreply.github.com>
2022-05-04 13:07:14 -0500

    Update renameFile.py

--- renameFile.py
+++ renameFile.py
@@ -1,8 +1,8 @@
 import os
 
 # Absolute path of a file
-old_name = r"/Users/shawnking/repo2/example.html"
-new_name = r"/Users/shawnking/repo2/index.html"
+old_name = r"/repo2/example.html"
+new_name = r"/repo2/index.html"
 
 # Renaming the file
 os.rename(old_name, new_name)
